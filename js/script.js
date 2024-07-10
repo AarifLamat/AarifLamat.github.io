9702c2510d654ce41277867d382688cbd7b5c89b
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2024-07-11 00:16:17 +0800

    somewhat functioning burgermenu nav

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    const burger = document.getElementById("burger-menu-container");
+    menu.classList.toggle("open");
+    burger.classList.toggle("open");
+    console.log("toggling menu");
+}

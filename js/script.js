7c4f3c117172464c63d168f7750529cd56e5b0f3
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2024-11-10 14:58:20 +0800

    added overlay to burger menu nav & rubik font

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu");
     const burger = document.getElementById("burger-menu-container");
+    const overlay = document.getElementById("overlay");
+    overlay.addEventListener("click", toggleMenu);
+    // document.body.appendChild(overlay);
+    overlay.classList.toggle("open");
     menu.classList.toggle("open");
     burger.classList.toggle("open");
     console.log("toggling menu");
 }
-
